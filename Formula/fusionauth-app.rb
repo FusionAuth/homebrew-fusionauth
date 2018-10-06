f91c6e365d57b4bd1c2189711e9cc150d3c98d17
Daniel DeGroff <daniel@inversoft.com>
2018-10-06 12:32:22 -0600

    add initial formulas

---
+++ Formula/fusionauth-app.rb
@@ -0,0 +1,17 @@
+lass FusionAuthApp < Formula
+  desc "FusionAuth App"
+  homepage "https://fusionauth.io"
+  url "https://storage.googleapis.com/inversoft_products_j098230498/products/fusionauth/1.0.17/fusionauth-app-1.0.17.zip"
+  sha256 "052c5acd5c75cc4d750fcaa940399fc2ec44c89d7c635973ded5f5897530ca03"
+
+  bottle :unneeded
+
+  def install
+    mkdir -p /usr/local/fusionauth/fusionauth-app
+    unzip -nq fusionauth-app-1.0.17.zip -d /usr/local/fusionauth
+  end
+
+  test do
+    ls /usr/local/inversoft/fusionauth-app
+  end
+end
---
+++ Formula/fusionauth-search.rb
@@ -0,0 +1,17 @@
+lass FusionAuthSearch < Formula
+  desc "FusionAuth Search"
+  homepage "https://fusionauth.io"
+  url "https://storage.googleapis.com/inversoft_products_j098230498/products/fusionauth/1.0.17/fusionauth-search-1.0.17.zip"
+  sha256 "6ff66f7291c007095cffa09c087a9824d588107c60988d38dd95a529ca7aaac8"
+
+  bottle :unneeded
+
+  def install
+    mkdir -p /usr/local/fusionauth/fusionauth-search
+    unzip -nq fusionauth-search-1.0.17.zip -d /usr/local/fusionauth
+  end
+
+  test do
+    ls /usr/local/inversoft/fusionauth-app
+  end
+end
