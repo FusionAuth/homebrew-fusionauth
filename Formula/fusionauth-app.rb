1a5a445e7de09a8e53e90d50e4325326fcd99b57
Tyler Scott <tyler@inversoft.com>
2019-02-13 17:03:20 -0700

    Updated and got the formulas working.

--- Formula/fusionauth-app.rb
+++ Formula/fusionauth-app.rb
@@ -1,13 +1,57 @@
 class FusionauthApp < Formula
   desc "FusionAuth App"
   homepage "https://fusionauth.io"
-  url "https://storage.googleapis.com/inversoft_products_j098230498/products/fusionauth/1.0.17/fusionauth-app-1.0.17.zip"
-  sha256 "052c5acd5c75cc4d750fcaa940399fc2ec44c89d7c635973ded5f5897530ca03"
+  url "https://storage.googleapis.com/inversoft_products_j098230498/products/fusionauth/1.4.0/fusionauth-app-1.4.0.zip"
+  sha256 "9b80a30c09cde7487e676181963005817aeb05d4bc9efb78bac5fcbda95c0f99"
 
   bottle :unneeded
 
+  depends_on :java => "1.8"
+
   def install
-    mkdir_p "fusionauth"
-    mv "fusionauth-app", "fusionauth/"
+    prefix.install "fusionauth-app"
+    etc.install "config" => "fusionauth" unless File.exist? etc/"fusionauth"
+    prefix.install_symlink etc/"fusionauth" => "config"
+  end
+
+  def post_install
+    (var/"log/fusionauth").mkpath
+  end
+
+  plist_options :manual => "fusionauth start"
+
+  def caveats; <<~EOS
+    Logs:   #{var}/log/fusionauth/fusionauth-app.log
+    Config: #{etc}/fusionauth/fusionauth.properties
+    EOS
+  end
+
+  # http://www.manpagez.com/man/5/launchd.plist/
+  def plist; <<~EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+      <key>KeepAlive</key>
+      <true/>
+      <key>Label</key>
+      <string>#{plist_name}</string>
+      <key>ProgramArguments</key>
+      <array>
+        <string>sh</string>
+        <string>catalina.sh</string>
+        <string>run</string>
+      </array>
+      <key>RunAtLoad</key>
+      <true/>
+      <key>WorkingDirectory</key>
+      <string>#{prefix}/fusionauth-app/apache-tomcat/bin</string>
+      <key>StandardOutPath</key>
+      <string>#{var}/log/fusionauth/fusionauth-app.log</string>
+      <key>StandardErrorPath</key>
+      <string>#{var}/log/fusionauth/fusionauth-app.log</string>
+    </dict>
+    </plist>
+  EOS
   end
 end
--- Formula/fusionauth-search.rb
+++ Formula/fusionauth-search.rb
@@ -1,17 +1,58 @@
 class FusionauthSearch < Formula
   desc "FusionAuth Search"
   homepage "https://fusionauth.io"
-  url "https://storage.googleapis.com/inversoft_products_j098230498/products/fusionauth/1.0.17/fusionauth-search-1.0.17.zip"
-  sha256 "6ff66f7291c007095cffa09c087a9824d588107c60988d38dd95a529ca7aaac8"
+  url "https://storage.googleapis.com/inversoft_products_j098230498/products/fusionauth/1.4.0/fusionauth-search-1.4.0.zip"
+  sha256 "385fbc5029bd7f61137c688a3965ce536b78d4b31653fbb7dc8fcad26870321c"
+
+  depends_on :java => "1.8"
 
   bottle :unneeded
 
+  keg_only "These libs are for internal commands, not for external linking."
+
   def install
-    #system "mkdir", "-p", "fusionauth/fusionauth-search"
-    #system "unzip", "-nq", "fusionauth-search-1.0.17.zip", "-d", "fusionauth"
+    prefix.install "fusionauth-search"
+    etc.install "config" => "fusionauth" unless File.exist? etc/"fusionauth"
+    prefix.install_symlink etc/"fusionauth" => "config"
   end
 
-  test do
-    system "ls", "fusionauth/fusionauth-search"
+  def post_install
+    (var/"log/fusionauth").mkpath
+  end
+
+  plist_options :manual => "fusionauth start"
+
+  def caveats; <<~EOS
+    Logs:   #{var}/log/fusionauth/fusionauth-search.log
+    Config: #{etc}/fusionauth/fusionauth.properties
+  EOS
+  end
+
+  # http://www.manpagez.com/man/5/launchd.plist/
+  def plist; <<~EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+      <key>KeepAlive</key>
+      <true/>
+      <key>Label</key>
+      <string>#{plist_name}</string>
+      <key>ProgramArguments</key>
+      <array>
+        <string>sh</string>
+        <string>./elasticsearch</string>
+      </array>
+      <key>RunAtLoad</key>
+      <true/>
+      <key>WorkingDirectory</key>
+      <string>#{prefix}/fusionauth-search/elasticsearch/bin</string>
+      <key>StandardOutPath</key>
+      <string>#{var}/log/fusionauth/fusionauth-search.log</string>
+      <key>StandardErrorPath</key>
+      <string>#{var}/log/fusionauth/fusionauth-search.log</string>
+    </dict>
+    </plist>
+  EOS
   end
 end
