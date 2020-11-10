c2f8038cfce0da35c65a89ccf6e934dd6ee25fd8
Daniel DeGroff <daniel@fusionauth.io>
2020-11-10 12:24:54 -0700

    Linux compatibility for CI

--- publish.sh
+++ publish.sh
@@ -3,6 +3,8 @@
 #
 # >  ./release.sh 1.10.1
 
+set -e
+
 # Download files
 if ! test -f "./fusionauth-app-${1}.zip"; then
   wget "https://storage.googleapis.com/inversoft_products_j098230498/products/fusionauth/${1}/fusionauth-app-${1}.zip"
@@ -13,15 +15,15 @@ if ! test -f "./fusionauth-search-${1}.zip"; then
 fi
 
 # Update URL
-sed -i '' -E "s/fusionauth\/(.*)\/fusionauth-app-(.*).zip\"$/fusionauth\/${1}\/fusionauth-app-${1}.zip\"/" ./Formula/fusionauth-app.rb
-sed -i '' -E "s/fusionauth\/(.*)\/fusionauth-search-(.*).zip\"$/fusionauth\/${1}\/fusionauth-search-${1}.zip\"/" ./Formula/fusionauth-search.rb
+sed -i -E "s/fusionauth\/(.*)\/fusionauth-app-(.*).zip\"$/fusionauth\/${1}\/fusionauth-app-${1}.zip\"/" ./Formula/fusionauth-app.rb
+sed -i -E "s/fusionauth\/(.*)\/fusionauth-search-(.*).zip\"$/fusionauth\/${1}\/fusionauth-search-${1}.zip\"/" ./Formula/fusionauth-search.rb
 
 # Update SHA 256 sum
 sum=`sha256sum ./fusionauth-app-${1}.zip|awk -F" " '{print $1}'`
-sed -i '' -E "s/sha256 \"(.*)\"$/sha256 \"${sum}\"/" ./Formula/fusionauth-app.rb
+sed -i -E "s/sha256 \"(.*)\"$/sha256 \"${sum}\"/" ./Formula/fusionauth-app.rb
 
 sum=`sha256sum ./fusionauth-search-${1}.zip|awk -F" " '{print $1}'`
-sed -i '' -E "s/sha256 \"(.*)\"$/sha256 \"${sum}\"/" ./Formula/fusionauth-search.rb
+sed -i -E "s/sha256 \"(.*)\"$/sha256 \"${sum}\"/" ./Formula/fusionauth-search.rb
 
 git add .
 git commit -m"release ${1}"
